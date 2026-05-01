6d4cf3d09ea0e227bcf063253f6291b0272c2fd0
Akhilesh Kumar <117252754+akhilesh-kumar-01@users.noreply.github.com>
2026-05-01 23:12:36 +0530

    fix: server cleanup and relative api url

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const path = require('path');
 
 dotenv.config();
 
